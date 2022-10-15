92da28e08fca99685322c05cbc950f7faf12cbc5
Fernando Tizziani Lang <fernandolang.falkon@gmail.com>
2022-10-15 19:40:02 -0300

    [006] - Adjust code smells

--- src/main/java/bank/system/Main.java
+++ src/main/java/bank/system/Main.java
@@ -1,10 +1,5 @@
 package bank.system;
 
-import bank.system.infrastructure.common.Status;
-import bank.system.domain.user.User;
-import bank.system.infrastructure.persistence.PostgresConnection;
-import bank.system.infrastructure.persistence.user.UserPostgresGateway;
-import bank.system.infrastructure.repository.UserPostgresRepository;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -12,9 +7,6 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
-    private UserPostgresRepository userPostgresRepository;
-    private UserPostgresGateway userPostgresGateway;
 
     public static void main(String[] args) {
         launch(args);
