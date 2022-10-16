ea873f2bc063f470706c6e4a6144aa3f2630f6bf
Fernando Tizziani Lang <fernandolang.falkon@gmail.com>
2022-10-15 23:14:30 -0300

    [008] - View loader and binding with fxml

--- src/main/java/bank/system/infrastructure/api/controller/LoginController.java
+++ src/main/java/bank/system/infrastructure/api/controller/LoginController.java
@@ -1,20 +1,20 @@
 package bank.system.infrastructure.api.controller;
 
 import bank.system.fx.components.FXController;
-import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 public class LoginController extends FXController {
 
-    public Button btnTest;
+    @FXML
+    private Button btnTest;
     private static Integer count = 0;
 
-    public void onClick(ActionEvent actionEvent) {
+    public void onClick() {
         btnTest.setText(count.toString());
         count++;
-        System.out.println("Hello fx");
     }
 
     @Override
